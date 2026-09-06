38a70e85ac0adfb7ef09ce0a1b8b4e18b99da8f3
MohanadEid98 <mohanadeid@gmail.com>
2026-09-06 05:31:14 +0300

    updated vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      'task-alb-168342689.us-east-1.elb.amazonaws.com'
-    ]
+    host: '0.0.0.0',
+    allowedHosts: true
   }
 })
